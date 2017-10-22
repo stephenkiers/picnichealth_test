4339e77b1ee534adb2fb24a317c8ec55362c6555
Stephen Kiers <stephen@stephenkiers.com>
2017-10-21 23:41:25 -0600

    simplified everything

--- src/app/AppState/actions.js
+++ src/app/AppState/actions.js
@@ -1,6 +1,5 @@
 import {app_state_constants} from "./app_state_reducers";
 
-
 export const reduxAppStateSetModalId = id => ({
     type: app_state_constants.APP_STATE_SET_MODAL_TYPE_ID,
     id,
--- src/app/AppState/app_state_reducers.js
+++ src/app/AppState/app_state_reducers.js
@@ -1,26 +1,12 @@
-import { Map, Set } from 'immutable'
+import {Map} from 'immutable'
 
 export const app_state_constants = {
-    APP_STATE_SET_MODAL_TYPE_ID: 'APP_STATE_SET_MODAL_TYPE_ID',
-    APP_STATE_UPDATE_STATS:'APP_STATE_UPDATE_STATS',
 };
 
-const app_state = function(state = Map({
-    modal_id: undefined,
-    stats: Map({
-        total_records: 0,
-        avg_per_hour: 0,
-        records_last_hour_ratio: 0,
-    })
-}), action) {
+const default_app_state = {};
+
+const app_state = function(state = Map(default_app_state), action) {
     switch (action.type) {
-        case app_state_constants.APP_STATE_SET_MODAL_TYPE_ID:
-            return state.set('modal_id', action.id);
-        case app_state_constants.APP_STATE_UPDATE_STATS:
-            return state
-                .setIn(['stats', 'total_records'], action.total_records)
-                .setIn(['stats', 'avg_per_hour'], action.avg_per_hour)
-                .setIn(['stats', 'records_last_hour_ratio'], action.records_last_hour_ratio);
         default:
             return state;
     }
@@ -28,7 +14,5 @@ const app_state = function(state = Map({
 
 export default app_state;
 
-export const getModalId = state =>
-    state.get('modal_id');
-export const getStats = state =>
-    state.get('stats');
+// export const getSomething = (state, id) =>
+//     state.get(id);
