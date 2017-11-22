b76db558522d9941fa3a997434cae57ef218aef1
Stephen Kiers <stephen@stephenkiers.com>
2017-11-21 21:23:52 -0700

    creating action

--- src/app/GdaxQuoter/actions.js
+++ src/app/GdaxQuoter/actions.js
@@ -2,16 +2,17 @@ import {thing_constants} from './things_reducers'
 import {api, endpoints} from "../constants";
 import {apiFetchPromise} from "../utils";
 
+
+
 export const getCurrencies = () => {
     return dispatch => {
         return apiFetchPromise(api(endpoints.PRODUCTS_LIST), {level:2})
             .then((res) => {
-                console.log(res)
-                // dispatch({
-                //     type: thing_constants.THING_DELETE,
-                //     id,
-                //     name,
-                // })
+                const currencies = Map();
+                for (let i = 0; i < res.length; i++) {
+                    const current = res[i];
+
+                }
             }, (err) => {
             console.log(err)
                 // dispatch(handleApiErrors(err))
